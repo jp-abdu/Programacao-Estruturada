c8b0769e4cf42a621e448298fcc72e4df69d0bc2
jp-abdu <160797333+jp-abdu@users.noreply.github.com>
2024-03-20 16:34:18 -0300

    Delete ac6 directory

--- ac6/Calculo Simples.py
+++
@@ -1,8 +0,0 @@
-dados1 = input()
-dados2 = input()
-
-dados1 = dados1.split(" ")
-dados2 = dados2.split(" ")
-
-preco = int(dados1[1]) * float(dados1[2]) + int(dados2[1]) * float(dados2[2])
-print(f"VALOR A PAGAR: R$ {preco:.2f}")
